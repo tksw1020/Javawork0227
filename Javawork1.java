f98e119b19fe45218c850f40590d667d6a3b6e2f
tksw1020 <79001626+tksw1020@users.noreply.github.com>
2021-02-28 14:56:48 +0900

    Update Javawork1.java

--- Javawork1.java
+++ Javawork1.java
@@ -5,7 +5,7 @@ public class Javawork1 {
 	public static void main(String[] args) {
 		// TODO 九九の表を作る iがかけられる数　ｊがかける数
 		for(int i=1;i<=9;i++) {
-		for(int j=1;j<=9;j++) {
+			for(int j=1;j<=9;j++) {
 		System.out.print(i*j+" ");	
 		}
 			System.out.println();
